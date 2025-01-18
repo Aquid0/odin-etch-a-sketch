9198ebea93d9aae53dc18ba8b5dd561feff97ac8
Matthew <aquidmatthewla@gmail.com>
2025-01-18 02:35:35 +0000

    random colours and button

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let container = document.querySelector(".container");
 
-for (let _ = 0; _ < 16 * 16; _++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-}
+let boxes = 16;
 
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "500px";
+container.style.height = "500px";
+container.style.border = "1px solid black";
+
+let button = document.querySelector("#box-button");
+button.addEventListener("click", () => { 
+    let boxes;
+    do {
+        let input = prompt("Please enter the number of boxes.");
+        boxes = parseInt(input);
+    } while (boxes < 16 || boxes > 100);
+    container.innerHTML = "";
+    for (let _ = 0; _ < boxes * boxes; _++) {
+        const s = document.createElement("div");
+        s.addEventListener("mouseenter", () =>  {
+            s.style.backgroundColor = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;            
+        })
+        s.style.flex = `1 0 calc(100% / ${boxes})`;
+        s.style.border = "1px solid black";
+        s.style.backgroundColor = "#BEBEBE";
+        container.appendChild(s);
+    }
+});
